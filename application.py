454271446f3e2fc44d84785eff2f4aaa64362b51
David Greenfield <dg2815@columbia.edu>
2016-04-16 15:54:27 -0400

    initial split

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
-from pyfunctions.meetup_func import group_dates
+from meetup_func import group_dates
+
 application = Flask(__name__)
 import json
 
@@ -10,7 +11,33 @@ def display():
     ids=f.readlines()
     ids=[i.replace('\n','') for i in ids]
     y,g= group_dates()
-    return render_template('livedisplay.html',events=json.dumps(ids),years=y,groups=g)
+    return render_template('coretemp.html',events=json.dumps(ids),years=y,groups=g,page='growth.html')
+
+@application.route('/legis')
+def legis():
+    f=open('groupids.txt')
+    ids=f.readlines()
+    ids=[i.replace('\n','') for i in ids]
+    y,g= group_dates()
+    return render_template('coretemp.html',events=json.dumps(ids),years=y,groups=g,page='legislation.html')
+
+@application.route('/projection')
+def projection():
+    f=open('groupids.txt')
+    ids=f.readlines()
+    ids=[i.replace('\n','') for i in ids]
+    y,g= group_dates()
+    return render_template('coretemp.html',events=json.dumps(ids),years=y,groups=g,page='projection.html')
+
+@application.route('/community')
+def community():
+    f=open('groupids.txt')
+    ids=f.readlines()
+    ids=[i.replace('\n','') for i in ids]
+    y,g= group_dates()
+    return render_template('coretemp.html',events=json.dumps(ids),years=y,groups=g,page='community.html')
+
+
 
 if __name__ == '__main__':
     application.run(debug=True)
